66971116e071f1767a7dd7733b9d1c3958d049c8
powellmad <madsnpowell@gmail.com>
2021-01-26 21:52:17 -0600

    changed the fish for loop to a .map method

--- scripts/aquariumFish/fishList.js
+++ scripts/aquariumFish/fishList.js
@@ -1,28 +1,15 @@
 import { sortFish } from './fishDataProvider.js'
 import { Fish } from './fish.js'
 
-
-let fishHTML = ""
-const buildFishHTML = (fishArray) => {
-    for (const fishObj of fishArray) {
-        fishHTML += Fish(fishObj)
-    }
-    return fishHTML
-}
-
 export const FishList = () => {
-    // Get a reference to the `<article class="content">` element
-    const contentElement = document.querySelector(".containerLeft")
-
-    const allFishArrays = sortFish()
     
-    buildFishHTML(allFishArrays)
+const contentElement = document.querySelector(".containerLeft")
+const allFishArrays = sortFish()
 
-    // Add to the existing HTML in the content element
-    contentElement.innerHTML += `
+contentElement.innerHTML += `
         <article class="containerLeft__fishList contentArticle">
             <h2>Aquarium Fish</h2>
-            ${fishHTML}
+            ${allFishArrays.map(fishObj => Fish(fishObj)).join("")}
         </article>
     `
 }
